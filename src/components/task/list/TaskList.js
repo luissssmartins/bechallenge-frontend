b9de4e7036e41d481757ec92fc4a2f936ca541b8
Luis Martins <chateadorn@gmail.com>
2023-07-28 20:50:45 -0300

    corrigindo problema de declaração e objetos

--- src/components/task/list/TaskList.js
+++ src/components/task/list/TaskList.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import axios from 'axios';
 import './style/style.css';
 
-const TaskList = ({ tasks, handleEdit, handleDelete }) => {
+const TaskList = ({ tasks }) => {
 
   const handleEdit = async (tasks) => {
 
@@ -12,11 +12,11 @@ const TaskList = ({ tasks, handleEdit, handleDelete }) => {
 
       try {
 
-        await axios.put(`/api/tasks/${task.id}`, {
+        await axios.put(`/api/tasks/${tasks.id}`, {
           name: newTask
         });
         
-        handleEdit(task.id, newTask)
+        handleEdit(tasks.id, newTask)
 
       } catch (error) {
         console.error('Erro ao editar tarefa: ', error);
@@ -24,7 +24,7 @@ const TaskList = ({ tasks, handleEdit, handleDelete }) => {
     }
   }
 
-  const handleDelete = async (task) => {
+  const handleDelete = async (tasks) => {
 
     const confirmation = window.confirm(
       `Tem certeza de que deseja excluir esta tarefa?`
@@ -34,9 +34,9 @@ const TaskList = ({ tasks, handleEdit, handleDelete }) => {
 
       try {
 
-        await axios.delete(`http://127.0.0.1:8000/api/tasks/${task.id}`);
+        await axios.delete(`http://127.0.0.1:8000/api/tasks/${tasks.id}`);
 
-        handleDelete(task);
+        handleDelete(tasks);
 
 
       } catch (error) {
@@ -48,11 +48,11 @@ const TaskList = ({ tasks, handleEdit, handleDelete }) => {
 
   return (
     <ul>
-      {tasks.map((task) => (
-        <li key={task.id}>
-          {task}
-          <button className="editar" onClick={() => handleEdit(task)}>Editar</button>
-          <button className="excluir" onClick={() => handleDelete(task)}>Excluir</button>
+      {tasks.map((tasks) => (
+        <li key={tasks.id}>
+          {tasks}
+          <button className="editar" onClick={() => handleEdit(tasks)}>Editar</button>
+          <button className="excluir" onClick={() => handleDelete(tasks)}>Excluir</button>
         </li>
       ))}
     </ul>
